6e0ef05ac67bfb402cc291574f221f9af5323a48
xingwg <xwg031459@163.com>
2023-09-17 08:11:05 +0000

    🎨 统一对齐

--- examples/unidiffuser/pytorch/libs/uvit_multi_post_ln_v1.py
+++ examples/unidiffuser/pytorch/libs/uvit_multi_post_ln_v1.py
@@ -28,7 +28,7 @@ def timestep_embedding(timesteps, dim, max_period=10000):
     """
     half = dim // 2
     freqs = torch.exp(-math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half).to(device=timesteps.device)
-    args = timesteps[:, None].float() * freqs[None]
+    args = timesteps[:, None].type(torch.float32) * freqs[None]
     embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
     if dim % 2:
         embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
